97dbb2b13fb898c73b0177a77509bb0a51051b10
phonethantzaw <phonethantzaw@gmail.com>
2023-02-21 00:37:50 +0800

    Add User Management

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,15 +14,15 @@ urlpatterns = [
     # path for contact us view
     path(route='contact', view=views.contact, name='contact'),
     # path for registration
-
+    path('registration/', views.registration_request, name='registration'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
+    
     # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
-
+    path('logout/', views.logout_request, name='logout'),
+    
     # path for dealer reviews view
-
+    path(route='', view=views.get_dealerships, name='index'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
